f10ed56646cd2ab9e2e34a89c6e105ca10c481af
BiHongBin <410362365@qq.com>
2020-03-24 22:43:47 +0800

    创建了登录界面 重置了CSS

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,18 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">111
-    <router-view/>
+    <!-- <img src="./assets/logo.png" />111 -->
+    <router-view />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
-}
+  name: "App"
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  font-size: 14px;
 }
 </style>
---
+++ src/pages/login/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>登录页</div>
+</template>
+
+<script>
+export default {
+  name: "login",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style scoped></style>
